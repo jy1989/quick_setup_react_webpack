c2dddb593c6cbfca1d6c816f458cbdc226b5bfa7
chen <github@chenjianyi.com>
2016-12-14 14:00:08 +0800

    change a to link

--- app/tpl.js
+++ app/tpl.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 class Tpl extends React.Component {
   constructor(props) {
     super(props);
@@ -10,7 +11,7 @@ class Tpl extends React.Component {
     <div className="container-fixed-width">
     	<div className="header navbar-fixed-top">
     		<div className="container-fixed-width clearfix">
-        	<h1 className="logo"><a href="/#/" title="知乎日报" className="link-logo">知乎日报</a></h1>
+        	<h1 className="logo"><Link to="/" title="知乎日报" className="link-logo">知乎日报</Link></h1>
     		</div>
 		</div>
 		<div className="main-content-wrap" style={{paddingTop:'30px'}}>
